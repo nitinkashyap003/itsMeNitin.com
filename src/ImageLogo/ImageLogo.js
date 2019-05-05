7950f3c4a7ccd7d9a9bc30d887b904682f0ccc48
Nitin Kashyap <kashyapnitin003@rediffmail.com>
2019-05-06 03:47:57 +0530

    Added Main header

---
+++ src/ImageLogo/ImageLogo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ImageLogo extends Component {
+
+    render(){
+        return(
+           <div> 
+             <img
+             alt="Nitin"
+             src="" />
+             <div></div>
+           </div> 
+        );
+    }
+}
+export default ImageLogo;
